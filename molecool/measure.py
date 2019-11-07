ae3eada4341e5056cbd5dfea98636555fa4bff16
Casey Hansen <casey@cl2-wifi-10-215-50-7.wireless.pitt.edu>
2019-11-07 15:55:48 -0500

    added docstring to calculate_distance

--- molecool/measure.py
+++ molecool/measure.py
@@ -1,17 +1,37 @@
 import numpy as np
 
 def calculate_distance(rA, rB):
-    # This function calculates the distance between two points given as numpy arrays.
-    d=(rA-rB)
-    dist=np.linalg.norm(d)
-    return dist
+    """
+    This function calculates the distance between two points
+
+    Parameters
+    ----------
+    rA, rB : np.ndarray
+        The coordinates of each point.
+
+    Returns
+    -------
+    distance : float
+        The distance between two vectors.
+
+    Examples
+    --------
+    >>> r1 = np.array([0,0,0])
+    >>> r2 = np.array([3.0,0,0,])
+    >>> calculate_distance(r1_r2)
+    3.0
+    """
+    
+    dist_vec = (rA-rB)
+    distance = np.linalg.norm(dist_vec)
+    return distance
 
 def calculate_angle(rA, rB, rC, degrees=False):
     # Calculate the angle between three points. Answer is given in radians by default, but can be given in degrees
     # by setting degrees=True
-    AB = rB - rA
-    BC = rB - rC
-    theta=np.arccos(np.dot(AB, BC)/(np.linalg.norm(AB)*np.linalg.norm(BC)))
+    ab_diff = rA - rB
+    bc_diff = rB - rC
+    theta = np.arccos(np.dot(ab_diff, bc_diff)/(np.linalg.norm(ab_diff)*np.linalg.norm(bc_diff)))
 
     if degrees:
         return np.degrees(theta)
