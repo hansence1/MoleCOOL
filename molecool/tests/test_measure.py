ffbbce0454519be159490c3f77c537734d162f60
Casey Hansen <casey@cl2-wifi-10-215-50-58.wireless.pitt.edu>
2019-11-08 14:11:36 -0500

    commented out buggy code

--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -28,13 +28,13 @@ def test_calculate_angle():
 
     assert expected_angle == calculated_angle
 
-@pytest.mark.parametrize("p1, p2, p3, expected_angle",[
-    (np.array([-np.sqrt(2)/2,np.sqrt(2)/2,0]),np.array([0,0,0]),np.array([1,0,0]),45),
-    (np.array([0,0,-1]),np.array([0,1,0]),np.array([1,0,0]),60),
-    (np.array([-np.sqrt(3)/2,(1./2.),0]),np.array([0,0,0]),np.array([1,0,0]),30)
-])
-def test_calculate_angle_many(p1, p2, p3, expected_angle):
+# @pytest.mark.parametrize("p1, p2, p3, expected_angle",[
+#     (np.array([-np.sqrt(2)/2,np.sqrt(2)/2,0]),np.array([0,0,0]),np.array([1,0,0]),45),
+#     (np.array([0,0,-1]),np.array([0,1,0]),np.array([1,0,0]),60),
+#     (np.array([-np.sqrt(3)/2,(1./2.),0]),np.array([0,0,0]),np.array([1,0,0]),30)
+# ])
+# def test_calculate_angle_many(p1, p2, p3, expected_angle):
 
-    calculated_angle = molecool.calculate_angle(p1,p2,p3,degrees=True)
+#     calculated_angle = molecool.calculate_angle(p1,p2,p3,degrees=True)
 
-    assert calculated_angle == pytest.approx(expected_angle)
+#     assert calculated_angle == pytest.approx(expected_angle)
